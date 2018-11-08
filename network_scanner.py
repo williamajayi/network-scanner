aa55a204691b3c6f8442e74e01950abe7bbad566
root <root@localhost.localdomain>
2018-11-08 06:08:41 +0000

    python v3 updates

--- network_scanner.py
+++ network_scanner.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
 
 import scapy.all as scapy
-import optparse
+import argparse
 
 def get_arguments():
-    parser = optparse.OptionParser()
-    parser.add_option("-t", "--target", dest="target", help="Target IP / IP range")
-    (options, arguments) = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--target", dest="target", help="Target IP / IP range")
+    options = parser.parse_args()
     if not options.target:
         parser.error("[-] Please specify a target to scan using -t or --target options, use --help for more info.")
     return options
 
 def scan(ip):
-    arp_req = scapy.ARP(pdst=ip)
-    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_broadcast = broadcast/arp_req
-    response = scapy.srp(arp_broadcast, timeout=1, verbose=False)[0]
+    arp_req = scapy.ARP(pdst=ip)    # get an arp request
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")    # Set the destination mac address
+    arp_broadcast = broadcast/arp_req   # combine the broadcast and request to send to the network
+    answered = scapy.srp(arp_broadcast, timeout=1, verbose=False)[0]    # (scapy.srp) send and respond + allow ether frame for the answered resquests
     client_list = []
-    for element in response:
+    for element in answered:
         client_dict = {
         "ip": element[1].psrc,
         "mac": element[1].hwsrc
@@ -26,13 +26,13 @@ def scan(ip):
     return client_list
 
 def show_clients(client_list):
-    x = "IP Address\t\t MAC Address\t\t Count\t Len\t MAC Vendor / Hostname"
+    x = "IP Address                MAC Address"
     print(x)
-    print("=" * (len(x) + (8 * 4)))
+    print("=" * (len(x) + 4))
     for element in client_list:
         print(element["ip"] + "\t\t" + element["mac"])
 
 if __name__ == "__main__":
     options = get_arguments()
-    client_list = scan(options.target)
-    show_clients(client_list)
+    target_ip = scan(options.target)
+    show_clients(target_ip)
